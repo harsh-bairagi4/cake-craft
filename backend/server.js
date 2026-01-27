97e902b4eaa2fbb3158c8f9db7cdbbfe7a5709a9
harsh-bairagi4 <harskpw592@gmail.com>
2026-01-27 17:58:08 +0530

    cart functionality added

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,19 @@ import express from 'express'
 import 'dotenv/config'
 import connectDB from './config/db.js'
 import userRouter from './routes/userRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
+app.use(express.json());
+
 connectDB();
 
 app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
 
 app.get("/", (req, res)=>{
     res.send("Hello");
