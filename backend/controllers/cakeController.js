dbc0e925ee37c7679ba77aa9c5f98fedb7967a7d
harsh-bairagi4 <harskpw592@gmail.com>
2026-01-28 01:16:30 +0530

    cakecontroller added

---
+++ backend/controllers/cakeController.js
@@ -0,0 +1,57 @@
+import cakeModel from "../models/cakeModel.js";
+import fs from 'fs'
+
+
+const addCake = async (req, res) =>{
+    let image_filename = `${req.file.filename}`;
+
+    const cake = new cakeModel({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        category: req.body.category,
+        image: image_filename
+    });
+    try {
+        await cake.save();
+        res.json({
+            success: true,
+            message: "Food Added"
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({success: false,
+            message: "Error"
+        })
+    }
+}
+
+const listCake = async (req, res) =>{
+    try {
+        const cakes = await cakeModel.find({});
+        res.json({
+            success: true,
+            data: cakes
+        })
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, 
+            message: "Error"
+        })
+    }
+}
+const removeCake = async (req, res) =>{
+    try{
+        const cake = await cakeModel.findById(req.body.id);
+        fs.unlink(`uploads/${cake.image}`, ()=>{});
+
+        await cakeModel.findByIdAndDelete(req.body.id);
+        res.json({success: true, message: "Cake Removed"});
+    }
+    catch(error){
+        console.log(error);
+        res.json({success: false, message: "Error"});
+    }
+}
+
+export {addCake, listCake, removeCake}
